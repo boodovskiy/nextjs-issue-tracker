fb9fad22d848735c43aebdf63a9746b538ee2047
boodovskiy <dzak1987@mail.ru>
2024-08-08 10:37:20 +0300

    Build the NavBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavBar />
         <main className='p-5'>{children}</main>
       </body>
     </html>
