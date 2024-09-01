9cdf914eff7e681248f459935ac0e0eec49f8841
boodovskiy <dzak1987@mail.ru>
2024-09-01 11:34:21 +0300

    Build the API to assign Issues

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -2,5 +2,12 @@ import { z } from "zod";
 
 export const issueSchema = z.object({
     title: z.string().min(3, "Title is required.").max(255),
-    description: z.string().min(5)
+    description: z.string().min(5).max(65535)
+});
+
+export const patchIssueSchema = z.object({
+    title: z.string().min(3, "Title is required.").max(255).optional(),
+    description: z.string().min(5).max(65535).optional(),
+    assignedToUserId: z.string().min(1, "AssigneToUserId is required.").max(255).optional().nullable(),
+
 });
